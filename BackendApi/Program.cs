0aadca30abe03561d5d746cf69d6b6b0915f2bac
Eric Araujo <ericalbuquerque8@hotmail.com>
2025-11-23 14:07:41 -0300

    subindo containers e dependencias

---
+++ BackendApi/Program.cs
@@ -0,0 +1,32 @@
+using Backend.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{    app.UseSwagger();
+     app.UseSwaggerUI();
+}
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        sqlOptions => sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 5,
+            maxRetryDelay: TimeSpan.FromSeconds(10),
+            errorNumbersToAdd: null
+        )
+    );
+});
+
+
+app.UseHttpsRedirection();
+
+app.Run();
